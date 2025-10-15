fcb1d5357dc5af39d776da824a2a3dac0d24caf8
Mangrum Mro <mangrummro6@gmail.com>
2025-10-15 11:24:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6426053963-b4e9a",
+  "appId": "1:138859926977:web:866914a2cd08d9a13f4b40",
+  "apiKey": "AIzaSyDPwmNNm6Bckkg_aDv4aFfBgesOWi_qllM",
+  "authDomain": "studio-6426053963-b4e9a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "138859926977"
+};
