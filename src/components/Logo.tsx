4cf0578f70caaf6abda2656b310b3cd8d5248c8a
Mangrum Mro <mangrummro6@gmail.com>
2025-10-15 11:27:47 +0000

    the logo will be quize related

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -17,8 +17,8 @@ export function Logo() {
         className="h-8 w-8 text-primary"
       >
         <circle cx="12" cy="12" r="10" />
-        <path d="M12 16v-4" />
-        <path d="M12 8h.01" />
+        <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
+        <path d="M12 17h.01" />
       </svg>
       <span className="text-xl font-semibold text-foreground hidden sm:inline-block">
         BD Quiz
